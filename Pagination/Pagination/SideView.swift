8d94ffea21e6a92680121e6930c9b1eed81271ed
DhruvSakariya29102003 <141928874+DhruvSakariya29102003@users.noreply.github.com>
2023-12-22 13:14:52 +0530

    update

---
+++ Pagination/Pagination/SideView.swift
@@ -0,0 +1,22 @@
+//
+//  SideView.swift
+//  Pagination
+//
+//  Created by Droadmin on 18/09/23.
+//
+
+import UIKit
+
+class SideView: UIView {
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var moviNameLbl: UILabel!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
